a880f87f4954644692eac014007eecb502cf806a
jongbelegen <jongbelegen@users.noreply.github.com>
2023-12-04 18:36:25 +0100

    No globals

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,11 +1,1 @@
-import FlareClient from './FlareClient';
-import catchWindowErrors from './browserClient';
-export { readLinesFromFile } from './stacktrace/fileReader';
-
-export const flare = new FlareClient();
-
-if (typeof window !== 'undefined' && window) {
-    window.flare = flare;
-}
-
-catchWindowErrors();
+export {default} from './FlareClient';
